4b07bb87fc6992e2238f98488bd6c140fd9f09d8
Роман <151183288+RomanR3H1Antonnikov@users.noreply.github.com>
2026-09-15 19:16:38 +0300

    Add: anagram-groups - submission-9

---
+++ Data Structures & Algorithms/anagram-groups/submission-9.py
@@ -0,0 +1,13 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dict1 = {}
+        for string in strs:
+            alph = [0] * 26
+            char_a = ord("a")
+            for char in string:
+                alph[ord(char) - char_a] += 1
+            kluch = tuple(alph)
+            if kluch not in dict1:
+                dict1[kluch] = []
+            dict1[kluch].append(string)
+        return list(dict1.values())
